351b7f09f0b00ddf8a10bf96cceabcc479df3710
Jagadeesh-Udugundla <jagadeeshu9951@gmail.com>
2022-10-24 07:00:27 +0000

    initial commi

---
+++ index.js
@@ -0,0 +1,8 @@
+const element = (
+  <div>
+    <h1 className="greeting">Hi Jagadeesh</h1>
+    <p className="style:color=blue">paragraph</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
+// export default element;
